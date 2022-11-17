20bfe003bba4242304df72dab748ea58bdf63ed5
_hogan.vu <72756889+HoganVu@users.noreply.github.com>
2022-11-17 11:56:07 +0700

    update Content

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
+
+//Side effects
+
 function Content() {
+    // 1.useEffect (callback)
+    // Gọi callback mỗi khi component re-render
+    // Gọi callback sau khi component thêm element vào DOM
+    // 2.useEffect (callback, [])
+    // 3.useEffect (callback, [deps])
+
+    // ---------------------------------
+    //1. callback luôn được gọi sau khi component mounted
+
+    const [title,setTitle] = useState('') 
+
+    useEffect(() => {
+        document.title = title
+    })
+    
     return (
-        <h1>Hi anh em F8</h1>
+        <div>
+            <input
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+          
+        </div>
     )
 }
 
